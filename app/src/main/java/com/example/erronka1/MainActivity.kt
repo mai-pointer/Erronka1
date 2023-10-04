6e496189baf952465f2491744d9eae5a7ca1ab34
Maider Sanchez <maidersanchezcasas@gmail.com>
2023-10-04 10:26:29 +0200

    wip mainmenu

--- app/src/main/java/com/example/erronka1/MainActivity.kt
+++ app/src/main/java/com/example/erronka1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.erronka1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.erronka1.R.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var select: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+
+        select = findViewById(id.btnCarta)
+
+        select.setOnClickListener(){
+
+        }
     }
 }
