e628dfc193f26a8ff8b41be88a202c8d2ec36d97
Maider Sanchez <maidersanchezcasas@gmail.com>
2023-10-06 12:47:59 +0200

    wip userinfo window

--- app/src/main/java/com/example/erronka1/MainActivity.kt
+++ app/src/main/java/com/example/erronka1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.erronka1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -7,15 +8,38 @@ import android.widget.Toast
 import com.example.erronka1.R.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var select: LinearLayout
+    private lateinit var menu: LinearLayout
+    private lateinit var delivery: LinearLayout
+    private lateinit var providers: LinearLayout
+    private lateinit var zeroWaste: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
         MenuNav.Crear(this)
-        select = findViewById(id.btnCarta)
+        menu = findViewById(id.btnCarta)
+        delivery = findViewById(id.btnDelivery)
+        providers = findViewById(id.btnProviders)
+        zeroWaste = findViewById(id.btnZeroWaste)
 
-        select.setOnClickListener {
-            Toast.makeText(this@MainActivity, "EL boton funciona", Toast.LENGTH_SHORT).show()
+
+        menu.setOnClickListener {
+            /*val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)*/
+        }
+
+        delivery.setOnClickListener {
+            /*val intent = Intent(this, DeliveryActivity::class.java)
+            startActivity(intent)*/
+        }
+
+        providers.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        zeroWaste.setOnClickListener {
+            /*val intent = Intent(this, ZeroWasteActivity::class.java)
+            startActivity(intent)*/
         }
     }
 }
