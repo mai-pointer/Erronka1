6c1181b007d76158208071f0b9092b1f1e0a3e9d
ik_2dm3 <ik_2dm3@H015110.NET>
2023-10-19 08:56:49 +0200

    estaciones finished

--- app/src/main/java/com/example/erronka1/PantallaInvierno.kt
+++ app/src/main/java/com/example/erronka1/PantallaInvierno.kt
@@ -2,11 +2,14 @@ package com.example.erronka1
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import java.util.Locale
 
 class PantallaInvierno : AppCompatActivity() {
 
@@ -15,6 +18,8 @@ class PantallaInvierno : AppCompatActivity() {
     private lateinit var verano: ImageView
     private lateinit var primavera: ImageView
     private lateinit var titulo: TextView
+    private lateinit var argazkia: ImageView
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -22,29 +27,77 @@ class PantallaInvierno : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.invierno)
 
-        invierno = findViewById(R.id.imageView4)
-        otoño = findViewById(R.id.otoño)
-        verano = findViewById(R.id.verano)
-        primavera = findViewById(R.id.primavera)
-        titulo = findViewById(R.id.textView)
-        val nuevoTamañoTexto = 40f
+        invierno = findViewById(R.id.inviernoo)
+        otoño = findViewById(R.id.otoñoo)
+        verano = findViewById(R.id.veranoo)
+        primavera = findViewById(R.id.primaveraa)
+        titulo = findViewById(R.id.textview)
+        argazkia = findViewById(R.id.imageView4)
+        val nuevoTamañoTexto = 30f
+        titulo.textSize = nuevoTamañoTexto
+
 
         val user = FirebaseAuth.getInstance().currentUser
         MenuNav.Crear(this, user)
 
-        titulo.textSize = nuevoTamañoTexto
+        val myseason = intent.getStringExtra("season")
+        when (myseason) {
+            getString(R.string.I) -> {
+                titulo.text=getString(R.string.I)
+                invierno.visibility = View.GONE
+                argazkia.setImageResource(R.drawable.invierno)
+            }
+            getString(R.string.P) -> {
+                titulo.text=getString(R.string.P)
+                primavera.visibility = View.GONE
+                argazkia.setImageResource(R.drawable.primavera)
+            }
+            getString(R.string.V) -> {
+                titulo.text=getString(R.string.V)
+                verano.visibility = View.GONE
+                argazkia.setImageResource(R.drawable.verano)
+            }
+            getString(R.string.O) -> {
+                titulo.text=getString(R.string.O)
+                otoño.visibility = View.GONE
+                argazkia.setImageResource(R.drawable.otono)
+            }
+        }
 
         otoño.setOnClickListener{
-            val Intent: Intent = Intent(this, PantallaOtoño::class.java)
-            startActivity(Intent)
+            titulo.text=getString(R.string.O)
+            otoño.visibility = View.GONE
+            primavera.visibility = View.VISIBLE
+            invierno.visibility = View.VISIBLE
+            verano.visibility = View.VISIBLE
+            argazkia.setImageResource(R.drawable.otono)
         }
         primavera.setOnClickListener{
-            val Intent: Intent = Intent(this, PantallaPrimavera::class.java)
-            startActivity(Intent)
+            titulo.text=getString(R.string.P)
+            primavera.visibility = View.GONE
+            invierno.visibility = View.VISIBLE
+            otoño.visibility = View.VISIBLE
+            verano.visibility = View.VISIBLE
+            argazkia.setImageResource(R.drawable.primavera)
+
         }
         verano.setOnClickListener{
-            val Intent: Intent = Intent(this, PantallaVerano::class.java)
-            startActivity(Intent)
+            titulo.text=getString(R.string.V)
+            verano.visibility = View.GONE
+            primavera.visibility = View.VISIBLE
+            otoño.visibility = View.VISIBLE
+            invierno.visibility = View.VISIBLE
+            argazkia.setImageResource(R.drawable.verano)
+
+        }
+        invierno.setOnClickListener{
+            titulo.text=getString(R.string.I)
+            invierno.visibility = View.GONE
+            primavera.visibility = View.VISIBLE
+            otoño.visibility = View.VISIBLE
+            verano.visibility = View.VISIBLE
+            argazkia.setImageResource(R.drawable.invierno)
+
         }
     }
 }
